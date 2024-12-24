0d2b3c3d94fb3ce4c5938a8379586607a5fc75a3
Lim Jitt Hing <jhlim.2023@smu.edu.sg>
2024-12-24 17:44:13 +0800

    frontend integration

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -15,7 +16,7 @@ import (
 )
 
 type Todo struct {
-	ID    primitive.ObjectID    `json:"id,omitempty" bson:"_id,omitempty"`
+	ID    primitive.ObjectID    `json:"_id,omitempty" bson:"_id,omitempty"`
 	Body  string `json:"body"`
 	Completed  bool   `json:"completed"`
 }
@@ -49,6 +50,10 @@ func main() {
 	collection = client.Database("go-react-crud").Collection("todos")
 
 	app := fiber.New()
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowHeaders: "Origin,Content-Type, Accept",
+	}))
 
 	app.Get("/api/todos", getTodos)
 	app.Post("/api/todos", createTodo)
